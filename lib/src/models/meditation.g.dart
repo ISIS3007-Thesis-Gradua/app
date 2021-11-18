45ec7588109023a1a0ceafd45ae8badbca46d27a
diegommezp28 <da.gomezp@uniandes.edu.co>
2021-11-17 20:08:09 -0500

    Meditation Adapter code

--- lib/src/models/meditation.g.dart
+++ lib/src/models/meditation.g.dart
@@ -1,38 +1,38 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'saved_meditation.dart';
+part of 'meditation.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
 // **************************************************************************
 
-class SavedMeditationAdapter extends TypeAdapter<SavedMeditation> {
+class SimpleMeditationAdapter extends TypeAdapter<SimpleMeditation> {
   @override
-  final int typeId = 1;
+  final int typeId = 0;
 
   @override
-  SavedMeditation read(BinaryReader reader) {
+  SimpleMeditation read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return SavedMeditation(
-      fields[0] as String,
-      fields[1] as String,
-      fields[2] as Duration,
+    return SimpleMeditation(
+      path: fields[0] as String,
+      name: fields[1] as String,
+      duration: fields[2] as Duration,
     );
   }
 
   @override
-  void write(BinaryWriter writer, SavedMeditation obj) {
+  void write(BinaryWriter writer, SimpleMeditation obj) {
     writer
       ..writeByte(3)
       ..writeByte(0)
       ..write(obj.path)
       ..writeByte(1)
-      ..write(obj.meditationName)
+      ..write(obj.name)
       ..writeByte(2)
-      ..write(obj.meditationDuration);
+      ..write(obj.duration);
   }
 
   @override
@@ -41,7 +41,7 @@ class SavedMeditationAdapter extends TypeAdapter<SavedMeditation> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is SavedMeditationAdapter &&
+      other is SimpleMeditationAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
