e7bc8b34519334302b47fd5263d602b00c421985
kaitsubaka <kaitsubaka@gmail.com>
2021-11-26 09:32:28 -0500

    code -> removed old backend

--- amplify/backend/function/Recomender/src/blueprints/home.py
+++
@@ -1,7 +0,0 @@
-from flask import  Blueprint, jsonify
-from ..services import home as home_logic
-home_bp = Blueprint('home', __name__, url_prefix='/home')
-
-@home_bp.route('/recommend')
-def home():
-    return jsonify(home_logic.home())
--- amplify/backend/function/Recomender/src/index.py
+++
@@ -1,8 +0,0 @@
-import awsgi
-from flask_cors import CORS
-from flask import Flask, jsonify, request
-from .app import create_app
-app = create_app()
-CORS(app)
-def handler(event, context):
-    return awsgi.response(app, event, context)
